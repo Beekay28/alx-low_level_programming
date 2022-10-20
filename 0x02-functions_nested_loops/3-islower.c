562b13d45f4845c2d1f1492ea78905371da9f716
Beekay28 <bonginkosigumede.bb@gmail.com>
2022-10-19 18:44:09 -0700

    3-islower.c3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,11 @@
 /**
  * _islower - checks if a character is a lowercase
  * @c: the character
- * Return: 1 if letter is lowercase, 0 if not 
+ * Return: 1 if letter is lowercase, 0 if not
  */
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
